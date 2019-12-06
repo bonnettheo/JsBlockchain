cbc120cc19622067f409a0023d273630467f7299
Bonnet <theo.bonnet1@etu.unice.fr>
2019-12-06 14:46:43 +0100

    without p2p blockchain

--- src/block.ts
+++ src/block.ts
@@ -1,4 +1,6 @@
-class Block {
+import {calculate_hash} from "./blockchain"; 
+
+export class Block {
 	
 	index: number;
 	previous_hash: string;
@@ -12,5 +14,19 @@ class Block {
 		this.timestamp = timestamp;
 		this.data = data;
 		this.hash = hash.toString();
+	}
+
+	is_valid = (previous_block) => {
+		if (previous_block.index + 1 !== this.index) {
+			console.log('invalid index');
+			return false;
+		} else if (previous_block.hash !== this.previous_hash) {
+			console.log('invalid previous hash');
+			return false;
+		} else if (calculate_hash(this.index, this.previous_hash, this.timestamp, this.data) !== this.hash) {
+			console.log('invalid hash');
+			return false;
+		}
+		return true;
 	}
 }
---
+++ src/blockchain.ts
@@ -0,0 +1,53 @@
+var CryptoJS = require("crypto-js");
+import {Block} from "./block";
+
+export var calculate_hash = (index, previous_hash, timestamp, data) => {
+	return CryptoJS.SHA256(index + previous_hash + timestamp + data).toString();
+};
+
+var genesis_block = () => {
+	return new Block(0, "0", 0, "first block", "0082f4e05d9e92b1e25099db67bf54cc584fb61a5d3f8560026ef9d7fec1f91b")
+}
+
+var blockchain = [genesis_block()];
+
+var get_latest_block = () => {
+	return blockchain[blockchain.length -1];
+}
+
+var generate_block = (data) => {
+	var previous_block = get_latest_block();
+	var next_index = previous_block.index + 1;
+	var previous_hash = previous_block.hash;
+	var next_timestamp = new Date().getTime() /1000;
+	var next_hash = calculate_hash(next_index, previous_hash, next_timestamp, data);
+	return new Block(next_index, previous_hash, next_timestamp, data, next_hash);
+}
+
+var is_valid_chain = (new_blocks) => {
+	if(JSON.stringify(new_blocks[0]) !== JSON.stringify(genesis_block())){
+		console.log(new_blocks[0]);
+		console.log(genesis_block());
+		return false;
+	}
+
+	for (var i = 1; i < new_blocks.length; i++) {
+		if (!new_blocks[i].is_valid(new_blocks[i-1])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+var replace_chain = (new_blocks) => {
+	if (is_valid_chain(new_blocks) && new_blocks.length > blockchain.length) {
+		console.log('new block chain accepted');
+		blockchain = new_blocks;
+		// broadcast(responseLatestMsg());
+	} else {
+		console.log('new blockchain refused');
+	}
+};
+
+console.log(generate_block("test"));
+console.log(is_valid_chain(blockchain));
